dc958ee20ed0bac0d72a41bc9d53881f9bd6ffe0
Aditya Yogish Pai <adityayogish@gmail.com>
2019-08-10 10:48:26 +0530

    using context manager

--- pdfsplitter.py
+++ pdfsplitter.py
@@ -1,17 +1,11 @@
 from PyPDF2 import PdfFileWriter,PdfFileReader
 
 def cropper(start,end,file):
-    inputPdf=PdfFileReader(open(file,"rb"))
-    outPdf=PdfFileWriter()
+    inputPdf = PdfFileReader(open(file,"rb"))
+    outPdf = PdfFileWriter()
+    with open(file.split(".")[0]+"cropped"+".pdf","wb") as ostream:
+        for page in range(start, end + 1):
+          outPdf.addPage(inputPdf.getPage(start))
+          outPdf.write(ostream)
 
-    ostream=open(file.split(".")[0]+"cropped"+".pdf","wb")
 
-    while start <= end:
-        outPdf.addPage(inputPdf.getPage(start))
-
-        outPdf.write(ostream)
-
-        start+=1
-    ostream.close()
-
-cropper(1,3,"sample.pdf")
