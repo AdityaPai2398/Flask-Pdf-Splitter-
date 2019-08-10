401d8215f3a23f9c262ba256fb336844e5710fe1
Aditya Yogish Pai <adityayogish@gmail.com>
2019-08-10 10:47:43 +0530

    removed global variables

--- app.py
+++ app.py
@@ -10,26 +10,24 @@ def upload():
 
 @app.route("/success",methods=["POST"])
 def success():
-    global st
-    global en
-    global file
-    st=int(request.form['start'])
-    en=int(request.form['end'])
+    success.start_page=int(request.form['start'])
+    success.end_page=int(request.form['end'])
     f=request.files['file']
-    file=f.filename
-    f.save(file)
-    return render_template("success.html",start=st,end=en,name=file)
+    success.file_name=f.filename
+    f.save(success.file_name)
+    return render_template("success.html",start=success.start_page,
+                           end=success.end_page,name=success.file_name)
 
 @app.route("/convert")
 def cropper():
-    pdfsplitter.cropper(st,en,file)
+    pdfsplitter.cropper(success.start_page,success.end_page,success.file_name)
     return render_template("download.html")
 
 
 
 @app.route("/download")
 def download():
-    filename=file.split(".")[0]+"cropped.pdf"
+    filename=success.file_name.split(".")[0]+"cropped.pdf"
     return send_file(filename,as_attachment=True)
 
 
